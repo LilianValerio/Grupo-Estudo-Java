f439e53deb4cb06509abd89703a95f041dbac161
Lilian Santos <liliansantos553@gmail.com>
2020-07-01 20:31:18 -0300

    primeiro commit

---
+++ src/com/desafio/um/MeuDesafioTeste.java
@@ -0,0 +1,17 @@
+package com.desafio.um;
+
+public class MeuDesafioTeste {
+
+	public static void main(String[] args) {
+	
+		int a = 6;
+		int b = 2;
+		int c = 7;
+			
+		int resultado = a + b;		
+		 		
+		System.out.println(resultado);
+
+	}
+
+}
